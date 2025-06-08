9d331c05dc7f61756b731fdc2b081151a58fc4f2
gorjan <gorjan200302@gmail.com>
2025-06-08 19:05:10 +0200

    Docker and docker-compose

--- backend/recipenest/settings.py
+++ backend/recipenest/settings.py
@@ -35,17 +35,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'recipes.apps.RecipesConfig',
     'rest_framework',
-
+    'corsheaders'
 ]
 from mongoengine import connect
 connect(
     db="recipenest_db",
-    host="mongodb://localhost:27017/recipenest_db",
+    host="mongo",
+    port=27017,
     alias="default"
 )
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -55,7 +57,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'recipenest.urls'
 
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 FRONTEND_DIR = BASE_DIR.parent / 'frontend'
