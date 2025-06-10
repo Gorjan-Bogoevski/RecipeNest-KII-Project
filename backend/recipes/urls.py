94570b028159d3a1ea2a7b01fddb78a2ee21fe75
gorjan <gorjan200302@gmail.com>
2025-06-10 12:03:35 +0200

    Test 7

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RecipeListCreateView, RecipeDetailView
 
 urlpatterns = [
-    path('api/recipes/', RecipeListCreateView.as_view(), name='recipe-list'),
-    path('api/recipes/<str:pk>/', RecipeDetailView.as_view(), name='recipe-detail'),
+    path('recipes/', RecipeListCreateView.as_view(), name='recipe-list'),
+    path('recipes/<str:pk>/', RecipeDetailView.as_view(), name='recipe-detail'),
 ]
